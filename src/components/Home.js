594e5c8707a2045c40bf032f4645544007d701ac
Fabuzac <fabuzac.novenaire@hotmail.com>
2022-04-23 16:09:14 +0200

    home design

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,11 +14,12 @@ class Home extends React.Component {
 			<>		
 				<Navbar/>
 				<div className='card p-4'>
-					<img width={50} src='/logo.svg' className='m-auto'></img>
+					<img width={100} src='/favicon.ico' className='m-auto'></img>
 				</div>
+				
 				<div className='m-auto row'>
 					{/* ARTICLE 1 */}
-					<div className="card border-primary mb-3 w-25 m-3">
+					<div className="card border-primary mb-3 w-25 m-3 col-sm h-25">
 						<div className="card-header">Header</div>
 						<div className="card-body">
 							<h4 className="card-title">Primary card title</h4>
@@ -27,14 +28,14 @@ class Home extends React.Component {
 								build on the card title and make up
 								the bulk of the card's content.						
 							</p>
-							<figcaption className="blockquote-footer">
+							<figcaption className="blockquote-footer mt-1">
 								Someone famous in <cite title="Source Title">Source Title</cite>
 							</figcaption>
 						</div>
 					</div>
 
 					{/* ARTICLE 2 */}
-					<div className="card border-primary mb-3 w-25 m-3">
+					<div className="card border-primary mb-3 w-25 m-3 col-sm h-25">
 						<div className="card-header">Header</div>
 						<div className="card-body">
 							<h4 className="card-title">Primary card title</h4>
@@ -43,28 +44,39 @@ class Home extends React.Component {
 								to build on the card title
 								and make up the bulk of the card's content.
 							</p>
-							<figcaption className="blockquote-footer">
+							<figcaption className="blockquote-footer mt-1">
 								Someone famous in <cite title="Source Title">Source Title</cite>
 							</figcaption>
 						</div>
 					</div>	
 
-					{/* ARTICLE 3 */}
-					<div className="card border-primary mb-3 w-25 m-3">
-						<div className="card-header">Header</div>
+					{/* LIST */}
+					<div className="card border-primary mb-3 w-25 m-3 w-25">
+						<div className="card-header">List</div>
 						<div className="card-body">
 							<h4 className="card-title">Primary card title</h4>
-							<p className="card-text">							
-								Some quick example text 
-								to build on the card title
-								and make up the bulk of the card's content.
-							</p>
-							<figcaption className="blockquote-footer">
+							<ul>
+								<li>Some quick</li>
+								<li>Example text</li>
+								<li>Card title</li>
+								<li>Make up the bulk</li>
+							</ul>
+							<figcaption className="blockquote-footer mt-1">
 								Someone famous in <cite title="Source Title">Source Title</cite>
 							</figcaption>
 						</div>
-					</div>						
-				</div>
+
+						<div className="card-body">
+							<h4 className="card-title">New Title List</h4>
+							<ul>
+								<li>Lorem</li>
+								<li>Ipsum Tatum</li>
+								<li>Card title</li>
+								<li>Make up the bulk</li>
+							</ul>
+						</div>
+					</div>		
+				</div>				
 			</>			
 		)
 	};
