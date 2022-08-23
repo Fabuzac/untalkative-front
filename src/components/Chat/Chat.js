cc8e58044ddbb30445e1bebaf9e201f4394f7465
Fabuzac <fabuzac.novenaire@hotmail.com>
2022-08-23 11:12:52 +0200

    chat message from DB, ok

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../Navbar/Navbar';
 import axios from 'axios';
+
+const headers = {
+	headers: {
+		'API-TOKEN': localStorage.getItem('token')
+	}
+}
 
 class Chat extends React.Component {
 	
@@ -9,28 +15,59 @@ class Chat extends React.Component {
 
 		this.state = {
 			message: '',
+			instantMessage: '',
 		}
 	}
 
 	handleSubmit = event => {
 		event.preventDefault()
-		
+
 		let bodyFormData = new FormData()
 		bodyFormData.set('message', this.state.message)
+		
+		// POST MESSAGE IN DB
+		axios.post('http://127.0.0.1:8000/api/chat', bodyFormData, headers)
+		.then( response => {
+
+			console.log("Message sent")
+			if(response.request.status === 200) {
+				//reset input value
+			}
+		})
+		.catch(error => {
+			console.log(error.response.data)
+		})
 	}
 
 	handleMessage = event => {
 		this.setState({ message: event.target.value }, () => {
-			// console.log(this.state)
+			console.log(this.state)
 		})
 	}
+
+	
+	componentDidMount() {
+		if(localStorage.getItem('token')) {
+      let id = this.props.match.params.id
+
+     // FETCH API MESSAGES
+      axios.get(`http://127.0.0.1:8000/api/chat/1`, headers)
+        .then(response => {
+					console.log("API OK " + response.status)
+					this.setState({instantMessage: response.data.body})
+        })        
+        .catch(error => {
+          console.log(error.response)
+        })
+    }
+	}
+
 
 	render() {
 		return (
 			<>
 				<Navbar/>
         <div className='row w-100'>
-
           <div className='col-3 text-left'>
             <h4 className='m-2'>Group list</h4>
 						<ul className='m-2'>
@@ -42,23 +79,28 @@ class Chat extends React.Component {
 
           <div className='col-6 border rounded border-dark vh-100 shadow-lg'>
             <h2 className='text-center my-2'>Chat Page <img className='w-7 rounded-circle' src='/Sho.png' alt='logo'></img></h2>
-						<div id='' className='text-left mx-5 my-1 bg-primary rounded'>
-							{ this.state.message
-								? <div>{this.state.message}</div>
-								: <div className='text-left mx-5 my-1 bg-primary rounded'>_</div>
+						{/* MESSAGES */}
+						
+						<div className='text-left mx-5 my-1 bg-primary rounded'>
+							{
+								this.state.instantMessage
 							}
 						</div>
-						<div id='' className='text-end mx-5 my-1 bg-warning rounded'>Hey, this is the second message</div>
+						<div className='text-end mx-5 my-1 bg-warning rounded'>
+							_
+						</div>
+
 						<div>
 							{/* FORM */}
 							<form method="POST" onSubmit={this.handleSubmit} encType="multipart/form-data">
 								<div className="form-group">
-									<label htmlFor="exampleInputEmail1">Message</label>
+									<label htmlFor="message">Message</label>
 									<input type="text" 
-												 id="exampleInputEmail1" 
+												 id="message" 
+												 name="message"
 												 onChange={this.handleMessage}
 												 aria-describedby="emailHelp"
-												 className="rounded mx-2" />
+												 className="rounded mx-2 w-50 p-1" />
 								</div>								
 								<button type="submit" className="btn btn-sm btn-primary">Send</button>
 							</form>
@@ -74,7 +116,6 @@ class Chat extends React.Component {
 							<li>User</li>
 						</ul>
           </div>
-
         </div>
 			</>
 		)
